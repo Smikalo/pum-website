1e8709c73b2c37faa5ae394f2c6385213c7943d8
smikalo <smrtkozyrev@gmail.com>
2025-10-22 20:43:54 +0200

    test 9

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return <main style={{padding:24,fontFamily:"system-ui"}}>
         <h1>PUM — Next.js is live 🔥</h1>
-        <p>If you see this, CI/CD worked.</p>
+        <p>If you see this, CI/CD worked, which is really cool.</p>
     </main>;
 }//
